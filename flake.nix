26e82172d2eecccd5b13fd18a8576c8251142ce7
Ryan Mulligan <ryan@ryantm.com>
2024-02-01 11:58:23 -0800

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  outputs = { nixpkgs }:
+  outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in
   {
-    devShells.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
       packages = [
         pkgs.hello
       ];
